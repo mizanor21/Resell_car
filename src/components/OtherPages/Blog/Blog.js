43c48b953f4bb9fce3a51397f6be27d75f7e5cbc
mizanor21 <mizanorrahman398@gmail.com>
2022-11-24 16:47:11 +0600

    Blog part added

--- src/components/OtherPages/Blog/Blog.js
+++ src/components/OtherPages/Blog/Blog.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import blog_bg from '../../../assets/logo/blog_1.webp';
+import './Blog.css'
 
 const Blog = () => {
     return (
-        <div>
-            this is blog area!!!
+        <div className='blog '>
+            <div className="container mx-5 md:mx-auto ">
+                <div className="flex">
+                    <div className="w-1/2 flex items-center">
+                        <div className="text-white">
+                            <h3 className='text-3xl font-bold'>Our Blog</h3>
+                            <p><Link to={'/'} className='hover:text-green-600 hover:font-bold'>Home </Link> / Blog</p>
+                        </div>
+                    </div>
+                    <div className="flex md:justify-end w-1/2 py-10">
+                        <img src={blog_bg} className='w-1/2 rounded-3xl' alt="" />
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/rotutes/PrivetRoute/PrivetRoute.js
+++ src/rotutes/PrivetRoute/PrivetRoute.js
@@ -1,13 +1,12 @@
 import React, { useContext } from 'react';
 import { Navigate, useLocation } from 'react-router-dom';
-import Loading from '../../components/OtherPages/Loading/Loading';
 import { AuthContext } from '../../contexts/AuthProvider';
 
 const PrivetRoute = ({ children }) => {
     const { user, loading } = useContext(AuthContext);
     const location = useLocation();
     if (loading) {
-        <Loading></Loading>
+        return <div className="min-h-screen"><progress className="progress w-full"></progress></div>
     }
     if (user) {
         return children
